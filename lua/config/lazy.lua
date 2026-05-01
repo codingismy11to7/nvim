8e75b00e7e00c90b841d5818e31a9c3cfbec2b47
Steven Scott <steven@codemettle.com>
2026-05-01 10:12:27 -0400

    add lang.rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,6 +46,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.nix" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
